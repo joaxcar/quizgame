a9e45c17aceceba9dad4ffd17d1f266cc7b8ca1a
Johan Carlsson <johan@aisle.se>
2019-10-18 17:33:54 +0200

    Testing to run game in browser

--- server.go
+++ server.go
@@ -4,13 +4,20 @@ import (
 	"database/sql"
 	"fmt"
 	"html/template"
+	"math/rand"
 	"net/http"
 )
 
+type player struct {
+	UserID string
+	solvedProbs []problem
+}
+
 type webGame struct{
 	Db *sql.DB
-	DoneID []string
+	DoneID []int
 	CurID int
+	ProbSet []int
 }
 
 type problem struct {
@@ -38,14 +45,16 @@ func getProblem(db *sql.DB, ID int) problem{
 
 	return prob
 }
-func (g webGame) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (g *webGame) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ans := r.FormValue("answer")
 	prob := getProblem(g.Db, g.CurID)
-	fmt.Println(g.CurID)
 	if ans == prob.Answer {
+		g.DoneID = append(g.DoneID, g.CurID)
 		g.CurID++
-		prob = getProblem(g.Db, g.CurID)
-		fmt.Println(g.CurID)
+		if g.CurID >= len(g.ProbSet) {
+			http.Redirect(w,r,"/result",http.StatusFound)
+		}
+		prob = getProblem(g.Db, g.ProbSet[g.CurID])
 	}
 	t,_ := template.ParseFiles("site.html")
 	err := t.Execute(w, prob)
@@ -55,7 +64,21 @@ func (g webGame) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 }
 func initiateServer(db *sql.DB) {
-	wg := webGame{Db:db, CurID:1}
-	http.Handle("/", wg)
-	http.ListenAndServe(":8888", nil)
+	wg := webGame{Db:db}
+	q := `SELECT id FROM quiz`
+	res, err := db.Query(q)
+	if err != nil {
+		fmt.Println("Database problems while selectiong all probs")
+	}
+	for res.Next() {
+		var probID int
+		res.Scan(&probID)
+		wg.ProbSet = append(wg.ProbSet, probID)
+	}
+	rand.Shuffle(len(wg.ProbSet), func(i, j int) {wg.ProbSet[i],wg.ProbSet[j] = wg.ProbSet[j],wg.ProbSet[i]})
+	fmt.Println(len(wg.ProbSet))
+	wg.CurID = 0
+	http.Handle("/game", &wg)
+	_ = http.ListenAndServe(":8881", nil)
+	fmt.Println("hej")
 }
